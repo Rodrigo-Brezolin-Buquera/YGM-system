b7666d3345e1ecb481c91b2a3ce4d91dfd5a52cb
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-08-08 08:40:54 -0300

    dotenv no firebase

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFcy2h2lVirgDfDYB8kIPfGKtuhoXzFro",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "ygm-control.firebaseapp.com",
   projectId: "ygm-control",
   storageBucket: "ygm-control.appspot.com",
   messagingSenderId: "387750336927",
-  appId: "1:387750336927:web:8933513bdb4fd0f0f645aa",
+  appId: process.env.REACT_APP_APP_ID,
   measurementId: "G-X0D2W9CFRW"
 };
 
