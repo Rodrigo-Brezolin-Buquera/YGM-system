02c45556d209e996a1d347c8326e83ee0afca755
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-09-01 15:46:50 -0300

    chakraui CalendarPage

--- src/pages/calendarPage/components/createClassForm/CreateClassForm.js
+++ src/pages/calendarPage/components/createClassForm/CreateClassForm.js
@@ -1,81 +1,89 @@
 import React from 'react'
 import { FormLine, Form } from './styled'
-import useForm from '../../../../hooks/useForm'
-import { Button, CircularProgress, TextField, Typography } from '@material-ui/core'
+import { useForm } from "react-hook-form";
+import {
+    FormErrorMessage,
+    FormLabel,
+    FormControl,
+    Input,
+    Button,
+    Select
+} from "@chakra-ui/react";
 import { DayOptions, StyleOptions, TeacherOptions } from '../../../../constants/selectOptions'
 import { createClass } from '../../../../services/requests/calendarRequests'
 
 const CreateClassForm = ({ loading, setLoading }) => {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+        reset
+    } = useForm();
 
-    const [form, onChange, cleanFields] = useForm({
-        name: "",
-        day: "",
-        time: "",
-        teacher: "",
-        date: ""
-    })
-
-    const onSubmitForm = (e) => {
-        e.preventDefault()
+    const onSubmit = (values) => {
         setLoading(true)
-        createClass(form, setLoading)
-        cleanFields()
+        createClass(values, setLoading)
+        reset()
     }
 
     return (
-        <Form onSubmit={onSubmitForm} >
-            <FormLine>
-                <select
-                    name="name"
-                    onChange={onChange}
-                    placeholder="Escolha um estilo"
-                    value={form.name}
-                    required
-                >
-                    <StyleOptions />
-                </select>
-                <select
-                    name="day"
-                    onChange={onChange}
-                    placeholder="Escolha um dia de aula"
-                    value={form.day}
-                    required
-                >
-                    <DayOptions />
-                </select>
-                <select
-                    name="teacher"
-                    onChange={onChange}
-                    placeholder="Escolha um professor"
-                    value={form.teacher}
-                    required
-                >
-                    <TeacherOptions />
-                </select>
-            </FormLine>
-            <TextField
-                name="time"
-                onChange={onChange}
-                value={form.time}
-                type="time"
-                required
-                fullWidth
-            />
-            <TextField
-                name="date"
-                onChange={onChange}
-                placeholder="Data de início"
-                value={form.date}
-                type="date"
-                required
-                fullWidth
-            />
-            <Button
-                type={"submit"}
-                variant={"contained"}
-                color={"secondary"}
-            >
-                {loading ? <CircularProgress color={"inherit"} size={24} ></CircularProgress> : <Typography>Criar aula</Typography>}
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.name || errors.day || errors.teacher || errors.time || errors.date}>
+                <FormLine>
+                    <Select
+                        id="name"
+                        placeholder="Escolha um estilo"
+                        {...register("name", {
+                            required: "Campo Obrigatório",
+                        })}
+                    >
+                        <StyleOptions />
+                    </Select>
+                    <Select
+                        id="day"
+                        placeholder="Escolha um dia de aula"
+                        {...register("day", {
+                            required: "Campo Obrigatório",
+                        })}
+                    >
+                        <DayOptions />
+                    </Select>
+                    <Select
+                        id="teacher"
+                        placeholder="Escolha um professor"
+                        {...register("teacher", {
+                            required: "Campo Obrigatório",
+                        })}
+                    >
+                        <TeacherOptions />
+                    </Select>
+                </FormLine>
+                <Input
+                    id="time"
+                    type="time"
+                    {...register("time", {
+                        required: "Campo Obrigatório",
+                    })}
+                />
+                <Input
+                    id="date"
+                    placeholder="Data de início"
+                    type="date"
+                    {...register("date", {
+                        required: "Campo Obrigatório",
+                    })}
+                />
+                <FormErrorMessage>
+                    {errors.name && errors.name.message}
+                    {errors.day && errors.day.message}
+                    {errors.teacher && errors.teacher.message}
+                    {errors.time && errors.time.message}
+                    {errors.date && errors.date.message}
+                </FormErrorMessage>
+
+            </FormControl>
+            <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+                Criar aula
             </Button>
         </Form>
     )
