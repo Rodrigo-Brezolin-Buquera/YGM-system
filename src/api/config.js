167f2491ef3a81a78791e01d7edaf73eda7c1937
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-02-28 18:07:51 -0300

    mudança para prod

--- src/api/config.js
+++ src/api/config.js
@@ -1,27 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {  getFirestore } from "firebase/firestore/lite";
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_TEST_API_KEY,
-    authDomain: "ygm-control.firebaseapp.com",
-    projectId: "ygm-control",
-    storageBucket: "ygm-control.appspot.com",
-    messagingSenderId: "387750336927",
-    appId: "1:387750336927:web:8933513bdb4fd0f0f645aa",
-    measurementId: "G-X0D2W9CFRW"
-};
+import { getFirestore } from "firebase/firestore/lite";
 
 // const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_PROD_API_KEY,
-//     authDomain: "yoga-mangala.firebaseapp.com",
-//     projectId: "yoga-mangala",
-//     storageBucket: "yoga-mangala.appspot.com",
-//     messagingSenderId: "137752975736",
-//     appId: "1:137752975736:web:12f4e14176781452bdd08d",
-//     measurementId: "G-KHZH2NY655"
-//   };
+//     apiKey: process.env.REACT_APP_TEST_API_KEY,
+//     authDomain: "ygm-control.firebaseapp.com",
+//     projectId: "ygm-control",
+//     storageBucket: "ygm-control.appspot.com",
+//     messagingSenderId: "387750336927",
+//     appId: "1:387750336927:web:8933513bdb4fd0f0f645aa",
+//     measurementId: "G-X0D2W9CFRW"
+// };
 
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_PROD_API_KEY,
+    authDomain: "yoga-mangala.firebaseapp.com",
+    projectId: "yoga-mangala",
+    storageBucket: "yoga-mangala.appspot.com",
+    messagingSenderId: "137752975736",
+    appId: "1:137752975736:web:dd24beb084f90b5abdd08d",
+    measurementId: "G-9E7ZLQW45W"
+};
 
 
 const app = initializeApp(firebaseConfig);
@@ -31,14 +32,14 @@ export const auth = getAuth(app);
 
 
 
-export const usersCol =  "users"
-export const contractsCol =  "contracts"
-export const plansCol =  "plans"
-export const calendarCol =  "calendar"
-export const checkinsCol =  "checkins"
-export const businessCol =  "business"
-export const stylesCol =  "styles"
-export const teachersCol =  "teachers"
+export const usersCol = "users"
+export const contractsCol = "contracts"
+export const plansCol = "plans"
+export const calendarCol = "calendar"
+export const checkinsCol = "checkins"
+export const businessCol = "business"
+export const stylesCol = "styles"
+export const teachersCol = "teachers"
 
 
 
