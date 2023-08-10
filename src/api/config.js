78b8e40c25be69b3743cd2603981969dd56536e7
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-08-10 15:30:34 -0300

    banco produção

--- src/api/config.js
+++ src/api/config.js
@@ -2,26 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_TEST_API_KEY,
-    authDomain: "ygm-control.firebaseapp.com",
-    projectId: "ygm-control",
-    storageBucket: "ygm-control.appspot.com",
-    messagingSenderId: "387750336927",
-    appId: "1:387750336927:web:8933513bdb4fd0f0f645aa",
-    measurementId: "G-X0D2W9CFRW"
-};
-
-
 // const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_PROD_API_KEY,
-//     authDomain: "yoga-mangala.firebaseapp.com",
-//     projectId: "yoga-mangala",
-//     storageBucket: "yoga-mangala.appspot.com",
-//     messagingSenderId: "137752975736",
-//     appId: "1:137752975736:web:dd24beb084f90b5abdd08d",
-//     measurementId: "G-9E7ZLQW45W"
+//     apiKey: process.env.NEXT_PUBLIC_TEST_API_KEY,
+//     authDomain: "ygm-control.firebaseapp.com",
+//     projectId: "ygm-control",
+//     storageBucket: "ygm-control.appspot.com",
+//     messagingSenderId: "387750336927",
+//     appId: "1:387750336927:web:8933513bdb4fd0f0f645aa",
+//     measurementId: "G-X0D2W9CFRW"
 // };
+
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_PROD_API_KEY,
+    authDomain: "yoga-mangala.firebaseapp.com",
+    projectId: "yoga-mangala",
+    storageBucket: "yoga-mangala.appspot.com",
+    messagingSenderId: "137752975736",
+    appId: "1:137752975736:web:dd24beb084f90b5abdd08d",
+    measurementId: "G-9E7ZLQW45W"
+};
 
 
 const app = initializeApp(firebaseConfig);
